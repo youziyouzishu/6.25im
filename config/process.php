fbbe46fa241b4bcbb1b4b5e6924525078d52383c
Cleaner <youziyouzishu@gmail.com>
2025-08-04 14:21:18 +0800

    refactor(admin): 更新错误页面路径和服务器端口
    - 修改403、404 和 500 错误页面中的资源路径，使用绝对路径替代相对路径
    - 更新服务器监听端口从 8787到 8793

--- config/process.php
+++ config/process.php
@@ -21,7 +21,7 @@ global $argv;
 return [
     'webman' => [
         'handler' => Http::class,
-        'listen' => 'http://0.0.0.0:8787',
+        'listen' => 'http://0.0.0.0:8793',
         'count' => cpu_count() * 4,
         'user' => '',
         'group' => '',
