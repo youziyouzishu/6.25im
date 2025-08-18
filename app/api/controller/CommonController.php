d2cb21bcf9ac16b08b0fe1b031edcdc5ae726132
Cleaner <youziyouzishu@gmail.com>
2025-08-18 09:40:18 +0800

    feat(api): 添加通用接口控制器- 新增 CommonController 类，实现以下功能：   - 获取轮播图列表   - 获取系统配置   - 获取版本信息 - 所有接口均无需登录验证

---
+++ app/api/controller/CommonController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\api\controller;
+
+use app\admin\model\Banner;
+use app\admin\model\Version;
+use app\api\basic\Base;
+use plugin\admin\app\model\Option;
+use support\Request;
+
+class CommonController extends Base
+{
+    protected array $noNeedLogin = ['*'];
+
+    #获取轮播图
+    function getBannerList(Request $request)
+    {
+        $rows = Banner::orderByDesc('weigh')->get();
+        return $this->success('成功',$rows);
+    }
+
+    #获取配置
+    function getConfig()
+    {
+        $config = Option::where('name', 'system_config')->value('value');
+        $config = json_decode($config);
+        return $this->success('成功', $config);
+    }
+
+    #获取版本信息
+    function getVersion()
+    {
+        $version = Version::latest()->first();
+        return $this->success('获取成功',$version);
+    }
+
+
+
+}
